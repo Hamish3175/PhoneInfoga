f2ead950949d36e9f77345189923d3152f6702fb
Hamish McInnes <hamishcraigmcinnes@gmail.com>
2019-10-04 12:51:13 +1000

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -5,5 +5,5 @@
 # @url    : https://github.com/sundowndev
 # @author : Raphael Cerveaux (sundowndev)
 
-google_api_key=''
-google_cx_id=''
+google_api_key='AIzaSyD1BuFrx6on7Kv7nym-o-wyIBB-pbaSjP0'
+google_cx_id='007207194442300274185:txzd4ztlgse'
